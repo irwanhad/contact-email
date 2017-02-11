258d4ed06d477609e586684ec4c88f985911919a
Irwan Desktop <xirwanx@gmail.com>
2017-02-11 09:34:04 +0700

    + folder app2, change index app

--- app/index.php
+++ app/index.php
@@ -10,7 +10,55 @@
       <div class="container">
         <div class="row">
           <div class="col-sm-12">
-            as
+            <div class="form-wrap">
+              <div class="status alert alert-success" style="display: none"></div>
+              <form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendmail.php">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <label>Name *</label>
+                        <input type="text" name="name" class="form-control" required="required">
+                      </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <label>Email *</label>
+                      <input type="email" name="email" class="form-control" required="required">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <label>Phone *</label>
+                      <input type="number" name="phone" class="form-control" required="required">
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <label>Company</label>
+                      <input type="text" name="company" class="form-control">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div class="form-group">
+                      <label>Message *</label>
+                      <textarea class="form-control" name="message" rows="7" required="required"></textarea>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <button type="submit" name="submit" class="btn btn-primary btn-lg btn-send" required="required">Send Message!</button>
+                    </div>
+                  </div>
+                </div>
+
+              </form>
+            </div>
           </div>
         </div>
       </div>
